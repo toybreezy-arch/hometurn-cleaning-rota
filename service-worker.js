67406daf71bf9e6002395956ef5c9a78be076b5f
toybreezy-arch <toybreezy@gmail.com>
2026-07-29 21:50:34 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE = 'hometurn-v1';
+const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(found => found || fetch(event.request))));
