0371c89d9c5df84b9a86623f0477c2bd45c94c4c
toybreezy-arch <toybreezy@gmail.com>
2026-08-07 22:18:31 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE = 'hometurn-v1';
+const CACHE = 'hometurn-v2';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(clients.claim()));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(found => found || fetch(event.request))));
